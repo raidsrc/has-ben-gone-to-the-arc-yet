28af3fcf12d56192504d52ba6cefa4c4db15f514
Ray Louis <x1337c@gmail.com>
2022-02-18 00:04:45 -0800

    why won't favicon work

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import '../styles/globals.css'
 import "../styles/output.css"
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} >
+    <Head>
+      <link rel="shortcut icon" href='../public/favicon2.ico' />
+    </Head>
+  </Component>
+
 }
 
 export default MyApp
