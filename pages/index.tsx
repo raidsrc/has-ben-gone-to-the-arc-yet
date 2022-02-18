7f9ef69d4e554ea44b97af093d0675993c379676
Ray Louis <x1337c@gmail.com>
2022-02-17 23:55:40 -0800

    update favicon and title

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,17 +7,22 @@ import { useSpring, animated } from 'react-spring'
 const Home: NextPage = () => {
   const [mouseOver, setMouseOver] = useState(false)
   const styles = useSpring({
-    from: {opacity: !mouseOver ? 0 : 1},
-    to: {opacity: !mouseOver? 1 : 0},
+    from: { opacity: !mouseOver ? 0 : 1 },
+    to: { opacity: !mouseOver ? 1 : 0 },
     config: {
       mass: 8,
       tension: 80,
-      friction: !mouseOver ? 200 : 40,
+      friction: !mouseOver ? 300 : 40,
     }
   })
   return (
     <div className='flex justify-center items-center w-screen h-screen bg-slate-600'>
-      <animated.div style={styles} className='text-9xl text-gray-300' onMouseEnter={() => setMouseOver(true)} onMouseLeave={() => setMouseOver(false)}>No</animated.div>
+      <Head>
+        <title>Has Ben gone to the ARC yet?</title>
+      </Head>
+      <animated.div style={styles} className='text-9xl text-gray-300' onMouseEnter={() => setMouseOver(true)} onMouseLeave={() => setMouseOver(false)}>
+        No
+      </animated.div>
     </div>
   )
 }
