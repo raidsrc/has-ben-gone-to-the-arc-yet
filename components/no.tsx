0e890876e29390ea54a0de16e6a14b94e1c18038
Ray Louis <x1337c@gmail.com>
2022-02-22 14:28:25 -0800

    pretty colors

--- components/no.tsx
+++ components/no.tsx
@@ -4,12 +4,26 @@ import { animated, useSpring } from "react-spring"
 function No() {
   const [mouseEntered, setMouseEntered] = useState(false)
   const styles = useSpring({
-    from: { opacity: !mouseEntered ? 0 : 1 },
-    to: { opacity: !mouseEntered ? 1 : 0 },
+    from: {
+      opacity: !mouseEntered ? 0 : 1,
+      color: "blue"
+    },
+    to: async (next, cancel) => {
+      await next({ opacity: !mouseEntered ? 1 : 0 })
+      await next({ color: "red" })
+      await next({ color: "orange" })
+      await next({ color: "yellow" })
+      await next({ color: "green" })
+      await next({ color: "blue" })
+      await next({ color: "indigo" })
+      await next({ color: "violet" })
+      await next({ color: "rgb(209,213,219)" })
+    },
     config: {
-      mass: 8,
-      tension: 80,
-      friction: !mouseEntered ? 300 : 40,
+      mass: 3,
+      tension: 30,
+      friction: !mouseEntered ? 10 : 20,
+      clamp: true,
     }
   })
   return (
